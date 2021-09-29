d3a49ede3f20131968089ff0a966c829e39abe62
vgauther <33629464+vgauther@users.noreply.github.com>
2021-09-29 18:59:30 +0200

    Update function.php

--- function.php
+++ function.php
@@ -1,10 +1,11 @@
 add_filter( 'body_class', 'add_role_to_body_class2' );
 function add_role_to_body_class2( $classes ) {
-		global $product;
+	global $product;
 		if ($product) {
 		$zz =  str_replace(",", ".",explode("&",explode("<bdi>",$product->get_price_html())[1])[0]);
 		if ( floatval(wc_get_price_including_tax( $product )) != floatval($zz)) {
 			$classes[] = 'no-price-set';
-		}}
-
+			}
+		}
 	return $classes;
+}
